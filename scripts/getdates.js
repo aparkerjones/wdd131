e5f20a57c3fec8e5f1611f9c1c36888d671d5ca7
aparkerjones <parkor.j@gmail.com>
2026-01-14 12:30:51 -0700

    adjusted some styles and content

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+(function () {
+  const yearEl = document.getElementById('currentyear');
+  if (yearEl) yearEl.textContent = new Date().getFullYear();
+
+  const lastModEl = document.getElementById('lastModified');
+  if (lastModEl) lastModEl.textContent = `Last modified: ${document.lastModified}`;
+})();
